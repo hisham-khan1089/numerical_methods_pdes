352a849be76e35f32ebc5587b4905fea773e897a
Hisham Khan <hikha9089@gmail.com>
2025-12-16 08:38:17 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Exploration of Numerical Methods for PDEs
+(Ongoing)
+
+This repository contains
+- My notes from chapter 6 of Haberman's Applied PDEs with Fourier Series and Boundary Value Problems
+- Simulations of some basic PDEs in Python, along with video files
+
+This project is ongoing, and the files in this will be updated as I continue to do 
+more research.
+
+## 
+
+The two key dependencies in this file are:
+```bash
+numpy
+matplotlib
+```
+
+These can be installed using pip
+
+## Creating Simulations
+
+
+## Heat Equation
---
+++ heat_equation/2D_heat.py
@@ -0,0 +1,98 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import time
+
+start = time.perf_counter()
+
+L = 50 # side length of square plate
+iterations = 500
+
+k = 1.0
+delta_x = 1
+
+delta_t = 0.25
+s = (k * delta_t) / (delta_x ** 2)
+
+if s > 0.25:
+    print("WARNING: Stability conditions have been violated")
+
+
+# generate square plate for each iteration
+u = np.empty((iterations, L, L))
+
+# initial conditions everywhere on plate
+u_init = 0.0
+
+# boundary conditions
+u_top = 100.0
+u_left = 100.0
+u_bottom = 100.0
+u_right = 100.0
+
+# set initial conditions onto grid
+u.fill(u_init)
+
+# set boundary conditions for all iterations
+u[:, (L-1):, :] = u_top
+u[:, :, :1] = u_left
+u[:, :1, 1:] = u_bottom
+u[:, :, (L-1):] = u_right
+
+def calculate(u):
+    """
+    Use partial difference equation for 2D heat equation
+    to determine solution for every time iteration in u
+    """
+    for k in range(0, iterations-1):
+        for i in range(1, L-1, delta_x):
+            for j in range(1, L-1, delta_x):
+                u[k+1][i][j] = s * (u[k][i+1][j] + u[k][i-1][j] + u[k][i][j+1] + u[k][i][j-1] - 4*u[k][i][j]) + u[k][i][j]
+
+    return u
+
+x = np.arange(L, step = delta_x)
+y = np.arange(L, step = delta_x)
+X, Y = np.meshgrid(x, y)
+
+u = calculate(u)
+
+fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
+
+def update_plot(k, u_k):
+    """
+    function that generates the plot for a given u_k
+
+    k: frame
+    u_k: the temperature profile at frame k
+    """
+    plt.cla()
+    ax.plot_surface(X, Y, u_k, cmap=plt.cm.viridis)
+
+    ax.set_title(f"Temperature of 2D plate at t = {k * delta_t:.3f} s")
+    ax.set_xlabel("x position")
+    ax.set_ylabel("y position")
+    ax.set_zlabel("Temperature (C)")
+
+    ax.set_xlim(0, L)
+    ax.set_ylim(0, L)
+    ax.set_zlim(0, 100)
+
+    return ax
+    
+def animate(k):
+    """
+    function that is passed to FuncAnimation that does the updating
+    k: frame
+    """
+    return update_plot(k, u[k])
+
+animation = FuncAnimation(fig=fig, func=animate, frames = iterations, interval = 30, repeat = False)
+animation.save(f'heat_sim_2D.mp4')
+# plt.show() # NOTE: Execution time will only be printed if you close the figure window 
+
+end = time.perf_counter()
+duration = end-start
+print(f"Execution time: {duration:.4f} seconds")
+
+
